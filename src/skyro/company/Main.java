5e7457d9ac188ce95d9de70c6d3b9746e1cb478e
katya <kotenokches@rambler.ru>
2022-01-30 08:52:49 +0300

    Домашнее задание 3.Исправленное

--- src/skyro/company/Main.java
+++ src/skyro/company/Main.java
@@ -11,7 +11,8 @@ public class Main {
         long d = 1;
         float i = 0.1f;
         double f = 5.1;
-        char g = 1549;
+        char g = 'a';
+        boolean h = true;
         //Задача №2
         double firstBoxerWeight = 78.2;
         double secondBoxerWeight = 82.7;
@@ -21,40 +22,38 @@ public class Main {
         System.out.println("Разница между весами бойцов " + differenceBoxerWeight );
         //Задача №3
         int bananas = 5;
-        int oneBananasWeight = 80;
+        int oneBananasWeightGr = 80;
         int milk = 200;
-        int oneMilkWeight = 105;
+        int oneMilkWeightGr = 105;
         int ice = 2;
-        int oneIceWeight = 100;
+        int oneIceWeightGr = 100;
         int eggs = 4;
-        int oneEggsWeight = 70;
-        int weightGramm = bananas*oneBananasWeight + milk*oneMilkWeight + ice*oneIceWeight + eggs*oneEggsWeight;
-        double weight = weightGramm/1000.0;
-        System.out.println("Вес спорт-завтрака "+ weightGramm + "гр");
-        System.out.println("Вес спорт-завтрака "+ weight + "кг");
-        //Задача №4
+        int oneEggsWeightGr = 70;
         int weightSportsman = 7;
-        int weightGram = 7*1000;
-        System.out.println(weightGram/250 + " дней уйдет на похудение, если терять каждый день по 250грамм.");
-        System.out.println(weightGram/500 + " дней уйдет на похудение, если терять каждый день по 500грамм.");
+        int weightSportsmanGr = 7*1000;
+        int day250Gr = weightSportsmanGr/250;
+        int day500Gr = weightSportsmanGr/500;
+        System.out.println(day250Gr + " дней уйдет на похудение, если терять каждый день по 250грамм.");
+        System.out.println(day500Gr + " дней уйдет на похудение, если терять каждый день по 500грамм.");
         //Задача №5
-        int masha = 67760;
-        int denis = 83690;
-        int kristina = 76230;
-        int masha10 = masha+(masha*10)/100;
-        int denis10 = denis+(denis*10)/100;
-        int kristina10 = kristina+(kristina*10)/100;
-        int annualIncomeMasha = masha * 12;
-        int annualIncomeDenis = denis * 12;
-        int annualIncomeKristina = kristina * 12;
-        int annualIncomeMasha10 = masha10 * 12;
-        int annualIncomeDenis10 = denis10 * 12;
-        int annualIncomeKristina10 = kristina10 * 12;
-        int differenceAnnualIncomeMasha = annualIncomeMasha10 - annualIncomeMasha;
-        int differenceAnnualIncomeDenis = annualIncomeDenis10 - annualIncomeDenis;
-        int differenceAnnualIncomeKristina = annualIncomeKristina10 - annualIncomeKristina;
-        System.out.println("Маша теперь получает "+ masha10 + ". Годовой доход вырос на "+ differenceAnnualIncomeMasha + "рублей.");
-        System.out.println("Денис теперь получает "+ denis10 + ". Годовой доход вырос на "+ differenceAnnualIncomeDenis + "рублей.");
-        System.out.println("Кристина теперь получает "+ kristina10 + ". Годовой доход вырос на "+ differenceAnnualIncomeKristina + "рублей.");
+        int mashaSalary = 67760;
+        int denisSalary = 83690;
+        int kristinaSalary = 76230;
+        int percent = 10;
+        int mashaNewSalary = mashaSalary+(mashaSalary*percent)/100;
+        int denisNewSalary = denisSalary+(denisSalary*percent)/100;
+        int kristinaNewSalary = kristinaSalary+(kristinaSalary*percent)/100;
+        int annualIncomeMasha = mashaSalary * 12;
+        int annualIncomeDenis = denisSalary * 12;
+        int annualIncomeKristina = kristinaSalary * 12;
+        int annualIncomeMashaNewSalary = mashaNewSalary * 12;
+        int annualIncomeDenisNewSalary = denisNewSalary * 12;
+        int annualIncomeKristinaNewSalary = kristinaNewSalary * 12;
+        int differenceAnnualIncomeMasha = annualIncomeMashaNewSalary - annualIncomeMasha;
+        int differenceAnnualIncomeDenis = annualIncomeDenisNewSalary - annualIncomeDenis;
+        int differenceAnnualIncomeKristina = annualIncomeKristinaNewSalary - annualIncomeKristina;
+        System.out.println("Маша теперь получает "+ mashaNewSalary + ". Годовой доход вырос на "+ differenceAnnualIncomeMasha + "рублей.");
+        System.out.println("Денис теперь получает "+ denisNewSalary + ". Годовой доход вырос на "+ differenceAnnualIncomeDenis + "рублей.");
+        System.out.println("Кристина теперь получает "+ kristinaNewSalary + ". Годовой доход вырос на "+ differenceAnnualIncomeKristina + "рублей.");
     }
 }
